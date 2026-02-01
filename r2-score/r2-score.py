5a016f00e64da210cdcd636684139fb5769b0374
Harpil70 <patelharpil58@gmail.com>
2026-02-01 13:56:12 +0530

    Add solution for r2-score

---
+++ r2-score/r2-score.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def r2_score(y_true, y_pred) -> float:
+    """
+    Compute R² (coefficient of determination) for 1D regression.
+    Handle the constant-target edge case:
+      - return 1.0 if predictions match exactly,
+      - else 0.0.
+    """
+    # Write code here
+    y_pred = np.array(y_pred)
+    y_true = np.array(y_true)
+    ss_res = np.sum((y_true - y_pred) ** 2)
+    ss_tot = np.sum((y_true - np.mean(y_true)) ** 2)
+
+    if ss_tot == 0:
+        if ss_res == 0:
+            return 1.0
+        else:
+            return 0.0
+
+    return 1 - (ss_res / ss_tot)
